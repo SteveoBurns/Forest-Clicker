f33a086576460a702b9007018e889af05cb439c8
SteveoBurns <steveo.burns.777@gmail.com>
2021-03-12 08:00:58 +1100

    Update lure bird button

--- Assets/Forest Clicker/Scripts/Controller.cs
+++ Assets/Forest Clicker/Scripts/Controller.cs
@@ -7,23 +7,27 @@ public class Controller : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI seedsText;
     [SerializeField] private TextMeshProUGUI seedsPerSecText;
-    [SerializeField] private TextMeshProUGUI seedsButtonText; 
+    [SerializeField] private TextMeshProUGUI seedsButtonText;
+    [SerializeField] private TextMeshProUGUI lureBirdText;     
 
     private double seeds;
     private double seedsValue;
+    private double lureBirdCost;
 
     // Start is called before the first frame update
     void Start()
     {
         seedsValue = 1;
+        lureBirdCost = 15;
     }
 
     // Update is called once per frame
     void Update()
     {
-        seedsText.text = "Seeds: " + seeds.ToString();
+        seedsText.text = "Seeds: " + seeds.ToString("F0");
         SeedsButtonText();
 
+        lureBirdText.text = "Lure a bird\nAdds 1 seed per click\nCost: " + lureBirdCost.ToString("F0");
     }
 
 
@@ -43,5 +47,16 @@ public class Controller : MonoBehaviour
     public void Click()
     {
         seeds += seedsValue;
+    }
+
+    public void LureBirdClick()
+    {
+        if (seeds >= lureBirdCost)
+        {
+            seedsValue++;
+            seeds -= lureBirdCost;
+            lureBirdCost *= 1.4;
+            
+        }   
     }
 }
